75b316003c075193307712a415949187ece728e9
Shivansh Mehta <64299373+shivdon@users.noreply.github.com>
2022-03-17 11:47:56 +0530

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -21,8 +21,8 @@ export const gettingMedia = async () => {
   } else {
     let constraints = {
       video: {
-        height: { min: 400, ideal: 1080, max: 2160 },
-        width: { min: 640, ideal: 1920, max: 3840 },
+        height: { min: 400, ideal: 1080, max: 4320 },
+        width: { min: 640, ideal: 1920, max: 7680 },
         aspectRatio: { exact: 1.777777778 },
         frameRate: { max: 60, min: 10 },
         facingMode: { ideal: "environment" },
